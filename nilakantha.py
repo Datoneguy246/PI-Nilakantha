82ab8fe6188662efb8e51570336b798eed54d8b9
Declan Scott <35856291+Datoneguy246@users.noreply.github.com>
2021-12-24 17:32:56 -0500

    initial

---
+++ nilakantha.py
@@ -0,0 +1,31 @@
+import sys
+from decimal import *
+
+def main(sum):
+    # Alternate between adding and subtracting
+    ADDING = True
+    # For the multiplication part of the series
+    MULTIPLY = 2
+
+    while True:
+        # Sick calculations bro
+        quotient = Decimal(4) / Decimal(MULTIPLY * (MULTIPLY + 1) * (MULTIPLY + 2))
+        if ADDING:
+            sum = sum + quotient
+        else:
+            sum = sum - quotient
+    
+        # Change the variables
+        MULTIPLY = MULTIPLY + 2
+        ADDING = not ADDING
+
+        # print
+        print(sum)
+
+# Get loop count
+if len(sys.argv) != 1:
+    print("Usage: python3 nilakantha.py")
+else:
+    getcontext().prec = 100
+    # Start the series with a sum of 3
+    main(Decimal(3))
